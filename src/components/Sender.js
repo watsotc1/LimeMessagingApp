05f554ebff8c30341934ad81deb3b2fdb550db45
watsotc1 <taylor.c.watson@vanderbilt.edu>
2021-11-24 16:16:34 -0600

    Updated log in page

--- src/components/Sender.js
+++ src/components/Sender.js
@@ -36,20 +36,9 @@ export default (props) => {
 
   return (
     <div>
-      <p>What is your id?</p>
-      <input value={sender} onChange={(e) => setSender(e.target.value)} />
-      <p>Avoid spaces and special characters.</p>
-      <p>
-        Paste your key pair below or <button onClick={handleGenerateClick}>generate</button> a new one.
-      </p>
-      <input
-        value={JSON.stringify(keyPair)}
-        onChange={handleKeyPairInputChange}
-      />
-      <p>
-        You need to save this key pair somewhere safe if you want to log in with
-        the same user id later.
-      </p>
+      <p>User ID: <input value={sender} onChange={(e) => setSender(e.target.value)} /> </p>
+      <p>User Key: <input value={JSON.stringify(keyPair)} onChange={handleKeyPairInputChange} /> </p>
+      <p> New User? <button onClick={handleGenerateClick}>Generate your own unique key</button> </p>
       <button onClick={handleSubmit}>Submit</button>
       <p>{error}</p>
     </div>
